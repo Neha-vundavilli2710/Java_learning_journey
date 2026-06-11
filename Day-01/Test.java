cb6dc0e88abe8e37512a9f809577c256684e77a6
Neha-vundavilli2710 <23p31a4272@acet.ac.in>
2026-06-12 01:17:14 +0530

    add Test.java with sample Java code and comments for learning purposes

---
+++ Day-01/Test.java
@@ -0,0 +1,44 @@
+public class Test {
+   public static void main(String[] args) {
+    // System.out.println("Hello World");
+
+
+        // int a = 10;
+        // int b = 20;
+        // int sum = a + b;
+        // System.out.println("The sum of a and b is: " + sum);
+
+        // int a = 10;
+        // int b = 20; 
+        // int sub = a - b;
+        // System.out.println("The difference of a and b is: " + sub);
+
+        // int a = 10;
+        // int b = 20;
+        // int mul = a * b;
+        // System.out.println("The product of a and b is: " + mul);
+
+        // int a = 10;
+        // int b = 20;  
+        // int div = a / b;
+        // System.out.println("The quotient of a and b is: " + div);
+
+        // System.out.println("I am learning Java programming language.");
+
+        // System.out.println("int a = 10; int b = 20; int x = a + b; System.out.println(x);");
+
+
+        // int a = 10;
+        // int b = 20;
+        // int x = a + b;
+        // System.out.println(x);
+
+        // System.out.println("Bob likes learning new programming languages.");
+
+        // int a = 10;
+        // int b = 20;
+        // int rem = b % a;
+        // System.out.println("The remainder of b divided by a is: " + rem);
+
+   }
+}
