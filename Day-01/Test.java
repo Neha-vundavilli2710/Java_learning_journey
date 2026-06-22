095cc39f1d5b7d8985bc465a818f2d2d3a45543a
Neha-vundavilli2710 <23p31a4272@acet.ac.in>
2026-06-22 15:27:54 +0530

    refactor Test.java to remove commented code and add sample output statements

--- Day-01/Test.java
+++ Day-01/Test.java
@@ -1,44 +1,14 @@
 public class Test {
    public static void main(String[] args) {
-    // System.out.println("Hello World");
-
-
-        // int a = 10;
-        // int b = 20;
-        // int sum = a + b;
-        // System.out.println("The sum of a and b is: " + sum);
-
-        // int a = 10;
-        // int b = 20; 
-        // int sub = a - b;
-        // System.out.println("The difference of a and b is: " + sub);
-
-        // int a = 10;
-        // int b = 20;
-        // int mul = a * b;
-        // System.out.println("The product of a and b is: " + mul);
-
-        // int a = 10;
-        // int b = 20;  
-        // int div = a / b;
-        // System.out.println("The quotient of a and b is: " + div);
-
-        // System.out.println("I am learning Java programming language.");
-
-        // System.out.println("int a = 10; int b = 20; int x = a + b; System.out.println(x);");
-
-
-        // int a = 10;
-        // int b = 20;
-        // int x = a + b;
-        // System.out.println(x);
-
-        // System.out.println("Bob likes learning new programming languages.");
-
-        // int a = 10;
-        // int b = 20;
-        // int rem = b % a;
-        // System.out.println("The remainder of b divided by a is: " + rem);
-
+    System.out.println("Hello world");
+    System.out.println("Neha vundavilli");
+    System.out.println("Aditya College of Engineering and Technology");
+    System.out.println("Btech CSE - AIML");
+    System.out.println("I am learning java programming language");
+    System.out.println("10");
+    System.out.println("!@#$%^&*()_+-=,./<>?:;\"`~");
+    System.out.println("This is a test program");
+    System.out.println("I am excited to learn java");
+    System.out.println("I have started my 90 days roadmap journey today");
    }
 }
